d3883968e305fdd283de81464388771b845d0433
Aleksandr Guido <iggelds@gmail,com>
2022-12-30 11:12:40 +0100

    Models added

---
+++ src/main/java/com/example/RestSensor/models/Sensor.java
@@ -0,0 +1,38 @@
+package com.example.RestSensor.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Sensor")
+public class Sensor implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(message = "Name should be between 3 and 30 characters")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
