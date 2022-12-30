b544d812cbe38b849d4ab1f2c28b028adf1d469a
Aleksandr Guido <iggelds@gmail,com>
2022-12-30 09:52:20 +0100

    Initial commit

---
+++ src/main/java/com/example/RestSensor/RestSensorApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSensorApplication.class, args);
+	}
+
+}
